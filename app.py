e6a6864bb1ff6c78d25ce4d5301c65c7d1ded63b
Liza <170400eliza@gmail.com>
2021-12-24 18:43:35 +0300

    that's all

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import requests
 from flask import Flask, render_template, request, redirect
 import psycopg2
 
+alphabet = 'qwertyuiopasdfghjklzxcvbnm1234567890()!@#$%*{}[]|\?/`_-+=.'
+
 app = Flask(__name__)
 
 conn = psycopg2.connect(database='service',
@@ -38,25 +40,50 @@ def login():
     return render_template('login.html')
 
 
+
 @app.route('/registration/', methods=['POST', 'GET'])
 def registration():
-    attention = 'Attention'
     if request.method == 'POST':
         name = request.form.get('name')
         login = request.form.get('login')
         password = request.form.get('password')
-        print(name)
-        if name != '' and login != '' and password != '':
+
+        list_ = [name, login, password]
+        for i, line in enumerate(list_):
+            list_[i] = check(line, i)
+            if list_[i] != 'good':
+                return render_template('registration.html', attention=list_[i])
+        if list_.count('good') == 3:
             cursor.execute('INSERT INTO users (full_name, login, password) VALUES (%s, %s, %s);',
-                           (str(name), str(login), str(password))
-                           )
+                           (str(name), str(login), str(password)))
             conn.commit()
 
             return redirect('/login/')
-        else:
-            return render_template('registration.html', attention=attention)
+
     return render_template('registration.html')
 
 
+def check(line, num):
+    if num == 1:
+        cursor.execute("SELECT login FROM users WHERE login=%s",
+                       (str(line),))
+        records = list(cursor.fetchall())
+        if len(records) != 0:
+            return 'Такой логин уже занят'
+    if len(line) == 0:
+        return 'Одна или несколько строк ничего не содержат'
+    elif len(line) <= 2:
+        return 'Одна или несколько строк слишком короткие, минимальная длина 3'
+    elif len(line) > 15:
+        return 'Длина одной или нескольких строк превышает 15 символов'
+    elif line.count(' ') != 0:
+        return 'Одна или несколько строк имеют пробелы'
+    else:
+        for s in line.lower():
+            if s not in alphabet:
+                return 'Одна или несколько строк имеют запрешённые символы'
+        return 'good'
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
